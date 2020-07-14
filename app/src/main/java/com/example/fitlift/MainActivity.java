c862ac03f514942f437f1233217b20621486e39b
James Talavera <jtalavera@fb.com>
2020-07-14 11:42:51 -0700

    Skeleton for workout fragment added

--- app/src/main/java/com/example/fitlift/MainActivity.java
+++ app/src/main/java/com/example/fitlift/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.fitlift;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.fitlift.databinding.ActivityMainBinding;
+import com.example.fitlift.fragments.WorkoutFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,24 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        // Fragment selection
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+
+                switch (menuItem.getItemId()) {
+                    default:
+                        fragment = new WorkoutFragment();
+                        break;
+                }
+
+                // handles inserting the fragment to the container in main activity
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
 
         // func to test login
 //        ParseUser user = ParseUser.getCurrentUser();
