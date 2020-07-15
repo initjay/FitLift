8537f8104525f4b1503046b865c1d4c241352299
James Talavera <jtalavera@fb.com>
2020-07-15 11:04:20 -0700

    WorkoutAdapter created

--- app/src/main/java/com/example/fitlift/adapters/WorkoutAdapter.java
+++ app/src/main/java/com/example/fitlift/adapters/WorkoutAdapter.java
@@ -1,4 +1,61 @@
 package com.example.fitlift.adapters;
 
-public class WorkoutAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.fitlift.R;
+import com.example.fitlift.Workout;
+import com.example.fitlift.databinding.ItemWorkoutBinding;
+
+import java.util.List;
+
+public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Workout> workouts;
+
+    public WorkoutAdapter(Context context, List<Workout> workouts) {
+        this.context = context;
+        this.workouts = workouts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemWorkoutBinding binding = ItemWorkoutBinding.inflate(LayoutInflater.from(context), parent, false);
+        //View view = binding.getRoot();
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Workout workout = workouts.get(position);
+        holder.bind(workout);
+    }
+
+    @Override
+    public int getItemCount() {
+        return workouts.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ItemWorkoutBinding binding;
+
+        public ViewHolder(@NonNull ItemWorkoutBinding b) {
+            super(b.getRoot());
+            binding = b;
+        }
+
+        public void bind(Workout workout) {
+            // Bind the workout data to the view elements
+            binding.tvTitle.setText(workout.getTitle());
+            binding.tvDate.setText(workout.getCreatedAt().toString());
+        }
+    }
 }
