628cbaecec4e199b5f843ee3d873ae0e86a53ca2
James Talavera <jtalavera@fb.com>
2020-07-20 16:58:52 -0700

    Reps entry addition added

--- app/src/main/java/com/example/fitlift/fragments/WorkoutDetailsFragment.java
+++ app/src/main/java/com/example/fitlift/fragments/WorkoutDetailsFragment.java
@@ -9,10 +9,15 @@ import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.example.fitlift.R;
@@ -28,6 +33,11 @@ import com.parse.SaveCallback;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+import static com.example.fitlift.R.id.etReps2;
+import static com.example.fitlift.R.id.rl_exercise_entry;
+import static com.example.fitlift.R.id.visible;
 
 // TODO fix back button to go back to workoutFragment
 
@@ -62,7 +72,7 @@ public class WorkoutDetailsFragment extends Fragment {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         Bundle bundle = getArguments();
@@ -72,10 +82,15 @@ public class WorkoutDetailsFragment extends Fragment {
             String title = bundle.getString("Title");
             String date = bundle.getString("Date");
             String exercise = bundle.getString("Exercise");
+            Integer weight = bundle.getInt("Weight");
+            ArrayList<Integer> reps = bundle.getIntegerArrayList("Reps");
 
             binding.etTitle.setText(title);
             binding.tvDate.setText(date);
-            binding.etExercise.setText(exercise);
+            binding.etExercise1.setText(exercise);
+            // TODO: WILL HAVE TO UPDATE TO ARRAY OF EXERCISES, WEIGHTS, AND REPS
+            binding.etWeight1.setText(weight.toString());
+            binding.etReps1.setText(reps.get(0).toString());
 
         } else {
             DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
@@ -88,9 +103,9 @@ public class WorkoutDetailsFragment extends Fragment {
             public void onClick(View view) {
                 Log.i(TAG, "Save button clicked");
                 // grab edit text data
-                String exercise = binding.etExercise.getText().toString();
-                Integer weight = Integer.valueOf(binding.etWeight.getText().toString());
-                Integer reps = Integer.valueOf(binding.etReps.getText().toString());
+                String exercise = binding.etExercise1.getText().toString();
+                Integer weight = Integer.valueOf(binding.etWeight1.getText().toString());
+                Integer reps = Integer.valueOf(binding.etReps1.getText().toString());
                 String title = binding.etTitle.getText().toString();
 
                 try {
@@ -99,6 +114,193 @@ public class WorkoutDetailsFragment extends Fragment {
                     Log.e(TAG, "Error in savePost:");
                     e.printStackTrace();
                 }
+            }
+        });
+
+        // Add more reps
+        binding.etReps1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                binding.etReps2.setVisibility(view.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        binding.etReps2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                binding.etReps3.setVisibility(view.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        binding.etReps3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                binding.etReps4.setVisibility(view.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        binding.etReps4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                binding.etReps5.setVisibility(view.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        binding.etReps5.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                binding.etReps6.setVisibility(view.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        binding.etReps6.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                binding.etReps7.setVisibility(view.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        binding.etReps7.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                binding.etReps8.setVisibility(view.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        binding.etReps8.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                binding.etReps9.setVisibility(view.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        binding.etReps9.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                binding.etReps10.setVisibility(view.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        // Add new exercise entry
+        binding.etExercise1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                // TODO: Add views dynamically
+//                LayoutInflater layoutInflater = getLayoutInflater().from(getContext());
+//                //RelativeLayout workoutEntry = (RelativeLayout) binding.rlExerciseEntry;
+//                RelativeLayout layout = (RelativeLayout) layoutInflater.inflate(R.layout.fragment_workout_details, null, false);
+//                RelativeLayout relativeLayout = binding.rlExerciseEntry;
+//
+//                RelativeLayout.LayoutParams params= new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+//                params.addRule(RelativeLayout.BELOW, binding.rlExerciseEntry.getId());
+//
+//                relativeLayout.setLayoutParams(params);
+//                relativeLayout.addView(layout);
+
+//                RelativeLayout relativeLayout = new RelativeLayout(getContext());
+//                EditText editText = new EditText(getContext());
+//                editText.setLayoutParams();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
 
@@ -133,11 +335,6 @@ public class WorkoutDetailsFragment extends Fragment {
                     return;
                 }
                 Log.i(TAG, "Post save was successful!!");
-
-                binding.etTitle.setText("");
-                binding.etExercise.setText("");
-                binding.etReps.setText("");
-                binding.etWeight.setText("");
 
                 FragmentManager fragmentManager = getFragmentManager();
                 Fragment fragment = new WorkoutFragment();
