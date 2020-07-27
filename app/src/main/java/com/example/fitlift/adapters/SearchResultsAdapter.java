eba63e9b56517945c32b7c427ab585577c7aed71
James Talavera <jtalavera@fb.com>
2020-07-27 16:09:29 -0700

    All methods implemented

--- app/src/main/java/com/example/fitlift/adapters/SearchResultsAdapter.java
+++ app/src/main/java/com/example/fitlift/adapters/SearchResultsAdapter.java
@@ -1,33 +1,62 @@
 package com.example.fitlift.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.example.fitlift.databinding.ItemSearchResultBinding;
+import com.parse.ParseUser;
+
+import java.util.List;
+
 public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdapter.ViewHolder> {
+
+    private Context context;
+    private List<ParseUser> users;
+
+    public SearchResultsAdapter(Context context, List<ParseUser> users) {
+        this.context = context;
+        this.users = users;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        ItemSearchResultBinding binding = ItemSearchResultBinding.inflate(LayoutInflater.from(context), parent, false);
+        return new ViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        ParseUser user = users.get(position);
+        holder.bind(user);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return users.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
+
+        ItemSearchResultBinding binding;
+
+        public ViewHolder(@NonNull ItemSearchResultBinding b) {
+            super(b.getRoot());
+            this.binding = b;
+        }
+
+        public void bind(ParseUser user) {
+            binding.tvSearchResultUsername.setText(user.getUsername());
+
+            if(user.getParseFile("profileImg") != null) {
+                Glide.with(context).load(user.getParseFile("profileImg").getUrl()).circleCrop().into(binding.ivSearchResultProfileImg);
+            }
         }
     }
 }
