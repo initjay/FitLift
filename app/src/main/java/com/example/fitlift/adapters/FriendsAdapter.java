515b931e00fdd150d9a124329df81e63b7f83e0c
James Talavera <jtalavera@fb.com>
2020-07-26 23:07:07 -0700

    Friend card design added

--- app/src/main/java/com/example/fitlift/adapters/FriendsAdapter.java
+++ app/src/main/java/com/example/fitlift/adapters/FriendsAdapter.java
@@ -1,17 +1,34 @@
 package com.example.fitlift.adapters;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.fitlift.WorkoutJournal;
+import com.parse.ParseUser;
+
+import java.util.List;
+
 public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.ViewHolder> {
 
+    private Context context;
+    // Use workoutJournals to populate recycler view with workouts belong to users
+    // apart of the friends relation
+    private List<WorkoutJournal> friends;
+
+    public FriendsAdapter(Context context, List<WorkoutJournal> friends) {
+        this.context = context;
+        this.friends = friends;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+
         return null;
     }
 
