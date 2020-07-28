24eb3a6880ea748ad526c35fb22b234a8bba55d0
James Talavera <jtalavera@fb.com>
2020-07-28 12:21:09 -0700

    Camera intent implemented

--- app/src/main/java/com/example/fitlift/fragments/MealDetailsFragment.java
+++ app/src/main/java/com/example/fitlift/fragments/MealDetailsFragment.java
@@ -1,14 +1,19 @@
 package com.example.fitlift.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,11 +25,13 @@ import com.example.fitlift.R;
 import com.example.fitlift.databinding.FragmentMealDetailsBinding;
 import com.parse.GetCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.io.File;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -33,10 +40,13 @@ import java.util.List;
 public class MealDetailsFragment extends Fragment {
 
     public static final String TAG = "MealDetailsFragment";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 24;
     private FragmentMealDetailsBinding binding;
     private ParseUser user = ParseUser.getCurrentUser();
     private boolean mealJournalUpdate = false;
     private MealJournal updateObject;
+    private File photoFile;
+    public String photoFileName = "photo.jpg";
 
     public MealDetailsFragment() {
         // Required empty public constructor
@@ -88,10 +98,15 @@ public class MealDetailsFragment extends Fragment {
                     String description = object.getMealDescription();
                     List<String> nutrients = object.getNutrients();
                     List<Integer> amounts = object.getAmounts();
+                    ParseFile mealImage = object.getImage();
 
                     binding.etTitleMealDetails.setText(title);
                     binding.tvDateMealDetails.setText(date);
                     binding.etMealDescription.setText(description);
+
+                    if (mealImage != null) {
+
+                    }
 
                     int nutrientSize = nutrients.size();
                     int amountSize = amounts.size();
@@ -137,7 +152,15 @@ public class MealDetailsFragment extends Fragment {
             LocalDateTime now = LocalDateTime.now();
             binding.tvDateMealDetails.setText(dtf.format(now));
         }
+        // listener to launch camera
+        binding.btnAttachImg.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
+        // listener to save post
         binding.btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -206,6 +229,42 @@ public class MealDetailsFragment extends Fragment {
                 }
             }
         });
+    }
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider.FitLift", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
     }
 
     private void savePostUpdate(String currTitle, String currMealDescription, List<String> nutrients, List<Integer> amounts, MealJournal updateObject) {
