a52bbd59507a74d485a252efbc13c6e50d4a5b19
James Talavera <jtalavera@fb.com>
2020-07-29 18:09:17 -0700

    Change location settings updated to receive location updates

--- app/src/main/java/com/example/fitlift/activities/MapsActivity.java
+++ app/src/main/java/com/example/fitlift/activities/MapsActivity.java
@@ -6,6 +6,7 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -14,15 +15,22 @@ import android.widget.Toast;
 
 import com.example.fitlift.PermissionUtils;
 import com.example.fitlift.R;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity
         implements OnMapReadyCallback,
@@ -43,9 +51,12 @@ public class MapsActivity extends FragmentActivity
      * Flag indicating whether a requested permission has been denied after returning in
      * {@link #onRequestPermissionsResult(int, String[], int[])}.
      */
+    public static final int REQUEST_CHECK_SETTINGS = 9669;
     private boolean permissionDenied = false;
     private GoogleMap mMap;
     private FusedLocationProviderClient fusedLocationClient;
+    private Location mCurrentLocation;
+    private LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +68,41 @@ public class MapsActivity extends FragmentActivity
         mapFragment.getMapAsync(this);
         // Needed to get the last known location
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        // Needed to update current location
+        createLocationRequest();
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
+                Toast.makeText(MapsActivity.this, "Location settings satisfied", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(MapsActivity.this,
+                                REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
     }
 
     /**
@@ -94,6 +140,7 @@ public class MapsActivity extends FragmentActivity
                 fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
+                        mCurrentLocation = location;
                         // got last known location
                         LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
                         // TODO: ADD ZOOM TO INITIAL LOCATION
@@ -112,6 +159,12 @@ public class MapsActivity extends FragmentActivity
             PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
                     Manifest.permission.ACCESS_FINE_LOCATION, true);
         }
+    }
+
+    protected void createLocationRequest() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
     @Override
